e8438a47bef10b367848d6b1208f05b7a6fd7747
thuca1992 <thuca92@gmail.com>
2018-01-05 17:51:26 +0700

    Add files via upload
    aaaaa

---
+++ abc/abc/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  abc
+//
+//  Created by nguyen.manh.tuanb on 1/5/18.
+//  Copyright © 2018 nguyen.manh.tuanb. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
